743cc14df9bcbbcba53f81bd295dc98256da25a4
Fujimoto <genfujimoto@gmail.comi>
2020-07-19 19:21:26 +0900

    整理

--- learning_cpp/5-11/code1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-//#include <array><cmath><vector><cstudio>
-
-int func1()
-{
-    std::cout << "Hello";
-    return 0;
-}
--- learning_cpp/template.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  int x = 0;
-  return 0;
-}
